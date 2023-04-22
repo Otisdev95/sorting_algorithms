4893ecff6f7307082a7ef7b64ef5e6109018ae7c
otis <olaliotis95@gmail.com>
2023-04-21 18:56:30 -0700

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return;
 
-	for (a = 0; a < size - 1; a++)
+	for (; a < size - 1; a++)
 	{
 		for (b = 0; b < size - a - 1; b++)
 		{
@@ -30,5 +30,4 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 	}
-	return (1);
 }

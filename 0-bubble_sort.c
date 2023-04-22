3102b8d09be3daea929f003b2e6d09839ef4aa04
otis <olaliotis95@gmail.com>
2023-04-21 18:31:42 -0700

    0x1B. C - Sorting algorithms & Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t a = 0, b = 0;
+	int bubble = 0;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		for (b = 0; b < size - a - 1; b++)
+		{
+			// swap array[b] and array[b + 1]
+			if (array[b] > array[b + 1])
+			{
+				bubble = array[b + 1];
+				array[b + 1] = array[b];
+				array[b] = bubble;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>0x1B. C - Sorting algorithms & Big O</h1>
+<p>
+A project done in C programming as part of the requirment in the ALX SWE program. The aim is to understand and implement sorting algorithms of different types and also, to be able to evaluate or determine the big O notations of the time complexity of each sort algorithm:
+<li>
+<ul>in the best case</ul>
+<ul>in the average case</ul>
+<ul>in the worst case</ul>
+</li>
+In this project, we also learned how to select the best sorting algorithm for a given input
+</p>
+<p>Author: Otis Olali</p>

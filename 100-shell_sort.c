3b9e95c649566a61fa8541ce9cbbc5641b49a919
otis <olaliotis95@gmail.com>
2023-04-24 02:32:08 -0700

    0x1B. C - Sorting algorithms & Big O

---
+++ 100-shell_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap_int - Swaps two integers in an array.
+ * @a: The first integer to be swapped.
+ * @b: The next integer to be swapped.
+ */
+
+void swap_int(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ *		using the Shell sort algorithm, using the Knuth
+ *		sequence.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t point, a, b;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (point = 1; point < (size / 3);)
+		point = point * 3 + 1;
+	for (; point >= 1; point /= 3)
+	{
+		for (a = point; a < size; a++)
+		{
+			b = a;
+			while (b >= point && array[b - point] > array[b])
+			{
+				swap_int(array + b, array + (b - point));
+				b -= point;
+			}
+		}
+		print_array(array, size);
+	}
+}

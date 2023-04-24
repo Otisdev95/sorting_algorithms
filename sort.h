22f76dade64910b96371119fb80993c712eab5cb
otis <olaliotis95@gmail.com>
2023-04-24 03:00:53 -0700

    0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -30,6 +30,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int f, int s);
 void lomuto_sort(int *array, size_t size, int f, int s);
+void swap_int(int *x, int *y);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -38,6 +39,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif /* SORT_H */
